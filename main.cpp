006ae972267a211d6ee4322ff42ab110764d9b04
bwang22 <ttwanksta@gmail.com>
2017-12-01 18:11:05 -0500

    change to posw-seeder

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ public:
   CDnsSeedOpts() : nThreads(96), nDnsThreads(4), nPort(53), mbox(NULL), ns(NULL), host(NULL), tor(NULL), fUseTestNet(false), fWipeBan(false), fWipeIgnore(false), ipv4_proxy(NULL), ipv6_proxy(NULL) {}
 
   void ParseCommandLine(int argc, char **argv) {
-    static const char *help = "pivx-seeder\n"
+    static const char *help = "posw-seeder\n"
                               "Usage: %s -h <host> -n <ns> [-m <mbox>] [-t <threads>] [-p <port>]\n"
                               "\n"
                               "Options:\n"
@@ -82,17 +82,17 @@ public:
           host = optarg;
           break;
         }
-        
+
         case 'm': {
           mbox = optarg;
           break;
         }
-        
+
         case 'n': {
           ns = optarg;
           break;
         }
-        
+
         case 't': {
           int n = strtol(optarg, NULL, 10);
           if (n > 0 && n < 1000) nThreads = n;
@@ -397,13 +397,13 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"seeder.fuzzbawls.pw", ""};
+static const string mainnet_seeds[] = {"ec2-54-159-73-159.compute-1.amazonaws.com", ""};
 static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
   if (!fTestNet){
-    db.Add(CService("188.165.212.82", 51472), true);
+    db.Add(CService("54.172.166.152", 62583), true);
   }
   do {
     for (int i=0; seeds[i] != ""; i++) {
@@ -455,10 +455,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0x90;
-      pchMessageStart[1] = 0xc4;
+      pchMessageStart[0] = 0x81;
+      pchMessageStart[1] = 0xc5;
       pchMessageStart[2] = 0xfd;
-      pchMessageStart[3] = 0xe9;
+      pchMessageStart[3] = 0xe8;
       seeds = testnet_seeds;
       fTestNet = true;
   }
